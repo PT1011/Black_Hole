d0961e0bad482aa117917e2107fc01dd5317af75
Parth Thite <parthpthite@gmail.com>
2026-08-10 17:16:45 -0400

    Added marker of black hole

--- black_hole.py
+++ black_hole.py
@@ -33,4 +33,5 @@ while Distance_From_Black_Hole > 0.9:
     Object_X_Path.append(Object_Position_X)
     Object_Y_Path.append(Object_Position_Y)
 
-    plt.plot(Object_X_Path, Object_Y_Path, color='blue')
+    plt.plot(Object_X_Path, Object_Y_Path, color='blue') # Plotting the path of the object
+    plt.plot(0, 0, marker='o', markersize=10, color='black')  # Black hole at the origin
